ab178eab25c3107c36e3a9fc45132596fbde8639
3RMc <ermakoff1994@mail.ru>
2018-02-03 12:53:04 +0300

    Директива модального окна и функция получения, показа/скрытия модального окна

--- app/script.js
+++ app/script.js
@@ -5,8 +5,25 @@ app.controller('MessagesListCtrl', function ($http) {
         .then(function (res) {
             vm.messages = res.data;
         });
+
     vm.addToFavourite = function (messageId) {
         var currentIndex = _.findIndex(vm.messages, { 'id': messageId });
-        vm.messages[currentIndex].favourite = (vm.messages[currentIndex].favourite === true) & false : true;
+        vm.messages[currentIndex].favourite = (vm.messages[currentIndex].favourite === true) ? false : true;
     };
-})
+
+    vm.openFull = function (messageId) {
+        var currentIndex = _.findIndex(vm.messages, { 'id': messageId });
+        vm.modalId = currentIndex;
+    };
+
+    vm.modalHide = function () {
+        vm.modalId = false;
+    };
+});
+
+app.directive('modalWindow', function (){
+    return {
+        restrict: 'E',
+        templateUrl: 'modal-window.html'
+    };
+});
